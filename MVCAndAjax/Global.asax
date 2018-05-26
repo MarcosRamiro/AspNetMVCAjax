9532126c5b42c45d6e68ec147236b15e1e503168
Marcos Ramiro dos Santos <mrsantos2006@hotmail.com>
2018-05-26 15:06:21 -0300

    Versao Inicial

---
+++ MVCAndAjax/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCAndAjax.MvcApplication" Language="C#" %>
---
+++ MVCAndAjax/Models/Usuario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVCAndAjax.Models
+{
+    public class Usuario
+    {
+        
+        public int Id { get; set; }
+        public string Login { get; set; }
+        public string Senha { get; set; }
+        public DateTime DataCadastro { get; set; }    
+    }
+}
